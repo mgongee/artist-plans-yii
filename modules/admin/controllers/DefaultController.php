4c0f191db6b4a714151ed33a8e4f71bb8d203ec1
mgongee <tanga.name@gmail.com>
2015-01-30 12:49:07 +0200

    Changed admin password and secured admin panel

--- modules/admin/controllers/DefaultController.php
+++ modules/admin/controllers/DefaultController.php
@@ -6,7 +6,13 @@ use yii\web\Controller;
 use yii\filters\AccessControl;
 
 class DefaultController extends AdminDefaultController
-{	
+{
+	
+	public function behaviors()
+    {
+		return $this->defaultBehaviors();
+    }
+	
     public function actionIndex()
     {
         return $this->render('index');
