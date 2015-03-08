81be95269be4c39570e6ecc0f6ae42bef74eb356
mgongee <tanga.name@gmail.com>
2015-03-08 22:44:40 +0200

    Removed mailer from composer and config; updated composer

--- config/web.php
+++ config/web.php
@@ -26,13 +26,13 @@ $config = [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        'mailer' => [
+      /*  'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
             'useFileTransport' => true,
-        ],
+        ],*/
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
             'targets' => [
